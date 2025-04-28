a99ae09c9f8b2dcebd5c18ff1aaf2d68ce36788d
Yuga Sun <yugasun.ai@gmail.com>
2025-04-28 16:06:03 +0800

    fix: vite build

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // support build for github pages
-  base: '/site/'
+  base: '/'
 })
