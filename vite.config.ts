26ff519be40c5db5dfc1762daacfc34185374396
Yuga Sun <yugasun.ai@gmail.com>
2025-04-28 15:15:55 +0800

    fix: vite build prefix

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // support build for github pages
+  base: '/site/'
 })
